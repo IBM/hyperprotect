206b7734af330f0f73d08c12fe28a757f6a76383
George Wilson <gcwilson@linux.ibm.com>
2021-06-16 23:18:06 -0500

    Add support for parsing ini file, authenticating, and parsing authtoken.
    Signed-off-by: George Wilson <gcwilson@linux.ibm.com>

--- dracut/85keyprotect-luks/keyprotect-luks.sh
+++ dracut/85keyprotect-luks/keyprotect-luks.sh
@@ -1,3 +1,86 @@
-#!/bin/sh
+#!/bin/sh -x
 
-echo "Hello from keyprotect-luks"
+parse_ini() {
+	while read INI_LINE; do
+		INI_LINE=${INI_LINE%%#*} # Eat comments
+		case $INI_LINE in
+			api_key*)
+				_API_KEY=`echo ${INI_LINE##*=}` # Take the RHS and let echo strip the space
+				;;
+			region*)
+				_REGION=`echo ${INI_LINE##*=}`
+				;;
+			endpoint_url*)
+				_ENDPOINT_URL=`echo ${INI_LINE##*=}`
+				;;
+			service_instance_id*)
+				_SERVICE_INSTANCE_ID=`echo ${INI_LINE##*=}`
+				;;
+			default_crk_uuid*)
+				_DEFAULT_CRK_UUID=`echo ${INI_LINE##*=}`
+				;;
+		esac
+	done < /etc/keyprotect-luks.ini
+}
+
+authenticate() {
+	_AUTH_JSON=`curl -X POST \
+	"https://iam.cloud.ibm.com/identity/token" \
+	--header 'Content-Type: application/x-www-form-urlencoded' \
+	--header 'Accept: application/json' \
+	--data-urlencode 'grant_type=urn:ibm:params:oauth:grant-type:apikey' \
+	--data-urlencode "apikey=$_API_KEY"`
+}
+
+parse_authtoken() {
+	#echo $_AUTH_JSON
+	#echo ${_AUTH_JSON##\{\"access_token\"\:\"}
+	_AUTH_TOKEN=${_AUTH_JSON##\{\"access_token\"\:\"}
+	_AUTH_TOKEN=${_AUTH_TOKEN%%\"*}
+}
+
+list_keys() {
+	curl -X GET \
+	$_ENDPOINT_URL/api/v2/keys \
+	-H 'accept: application/vnd.ibm.kms.key+json' \
+	-H "authorization: Bearer $_AUTH_TOKEN" \
+	-H "bluemix-instance: $_SERVICE_INSTANCE_ID"
+
+}
+
+unwrap_key() {
+	curl -X POST \
+	$_ENDPOINT_URL/api/v2/keys/$_DEFAULT_CRK_UUID/actions/unwrap \
+	-H 'accept: application/json' \
+	-H "authorization: Bearer $_AUTH_TOKEN" \
+	-H "bluemix-instance: $_SERVICE_INSTANCE_ID" \
+	-H 'content-type: application/vnd.ibm.kms.key_action_unwrap+json' \
+	-d "{
+		    \"ciphertext\": \"$_CIPHERTEXT\"
+  	    }"
+}
+
+if grep -q "rd.keyprotect-luks" /proc/cmdline; then
+	echo "Hello from keyprotect-luks"
+	parse_ini
+	echo "api_key = $_API_KEY"
+	echo "region = $_REGION"
+	echo "endpoint_url = $_ENDPOINT_URL"
+	echo "service_instance_id = $_SERVICE_INSTANCE_ID"
+	echo "default_crk_uuid = $_DEFAULT_CRK_UUID"
+	authenticate
+	parse_authtoken
+	echo $_AUTH_TOKEN
+	list_keys
+	_CIPHERTEXT=`cat /var/lib/keyprotect-luks/user/luks:root`
+	unwrap_key
+else
+	echo "keyprotect-luks says rd.keyprotect-luks is not set on the cmdline"
+fi
+
+#/usr/bin/curl http://crimini9.aus.stglabs.ibm.com/testpage.html
+
+#region = us-east # Another comment
+#service_instance_id = 8b12b984-5c42-4324-8a50-c4bc4a223154
+#endpoint_url = https://api.us-east.hs-crypto.cloud.ibm.com:9730
+#default_crk_uuid = 1cbf4897-de35-48d2-b393-eec5adf117a0
--- dracut/85keyprotect-luks/module-setup.sh
+++ dracut/85keyprotect-luks/module-setup.sh
@@ -14,4 +14,6 @@ depends() {
 install() {
 	inst_hook pre-mount 20 "$moddir/keyprotect-luks.sh"
 	inst_simple keyprotect-luks.sh "$moddir/keyprotect-luks.sh"
+	inst_simple /etc/keyprotect-luks.ini
+	inst_simple /var/lib/keyprotect-luks/user/luks:root
 }
